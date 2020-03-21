3d7ca7d67282a793f9f5bb61d6c92dc9b645ddbb
Yashagostov <yashagostov626@gmail.com>
2020-03-21 05:59:46 +0300

    drop on componnents

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+   return <header className="header">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXv6-QXmidx_W9OUEU1OO7LazrRZTn_s-yBfpMndZ8NnbpMaPV"></img>
+</header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+   return <div className="content">
+   <div>
+    <img src="http://fotorelax.ru/wp-content/uploads/2016/03/Beautiful-photos-and-pictures-on-various-subjects-01-1024x640.jpg"></img>
+    </div>
+    <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQkEv-qnBqsUcDsmv7YgbjYzI1v0kdkQ404A0QuQ6EgXjWiXQ6z"></img>
+      ava + description
+    </div>
+    <div>
+        my posts
+        <div>
+            new posts
+        </div>
+        <div>
+            <div>
+                post1
+                </div>
+            <div>
+                post3
+                </div>
+            <div>
+                post2
+                </div>
+        </div>
+    </div>
+</div>
+}
+
+export default Profile
