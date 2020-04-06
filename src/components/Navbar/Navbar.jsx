9f70bc326463e23561240ecaa08799b2e9948322
Yashagostov <yashagostov626@gmail.com>
2020-04-06 03:16:33 +0300

    add link users in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,6 +12,9 @@ const Nav = () => {
             <NavLink to='Dialogs' activeClassName={s.active}>Messages</NavLink>
         </div>
         <div className={s.item}>
+            <NavLink to='Users' activeClassName={s.active}>Users</NavLink>
+        </div>
+        <div className={s.item}>
             <NavLink to='News' activeClassName={s.active}>News</NavLink>
         </div>
         <div className={s.item}>
