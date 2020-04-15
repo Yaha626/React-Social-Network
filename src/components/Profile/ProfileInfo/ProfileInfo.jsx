c48afabbe7fd334f47e74ee952ec8f5f11901298
Yashagostov <yashagostov626@gmail.com>
2020-04-15 07:36:49 +0300

    local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from '../../Сommon/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus.jsx';
+
 
 
 const ProfileInfo = (props) => {
@@ -9,12 +11,12 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
+            {/* <div>
                 <img className={s.src} src="https://skinali-market.ru/upload/resize_cache/iblock/a48/1920_531_156420db5d35cfb82f16501d0f4565d1d/a483c3cadef147812418e4072c7033a2.jpg"></img>
-            </div>
+            </div> */}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} ></img>
-                ava+description
+               <ProfileStatus status={'Hello my friends'}/>
             </div>
         </div>
     )
