977fb164c2b107b2c5a59620ab88913d9ba0d6b6
Yashagostov <yashagostov626@gmail.com>
2020-05-03 09:10:21 +0300

    update paginator

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Paginator from '../Сommon/components/Paginator/Paginator';
 import User from './User';
 
-let Users = (CurrentPage, onPageChanged, totalUsersCount, pageSize, users, ...props) => {
+let Users = (CurrentPage, onPageChanged, totalItemsCount, pageSize, users, ...props) => {
     return <div >
         <Paginator CurrentPage={CurrentPage} onPageChanged={onPageChanged}
-            totalUsersCount={totalUsersCount} pageSize={pageSize} />
+            totalItemsCount={totalItemsCount} pageSize={pageSize} />
         <div>
             {
-                users.map(u => <User user={u} key={u.id}
+                users.map(u => <User user={u}
                     followingInProgress={props.followingInProgress}
-                    unfollow={props.unfollow} follow={props.follow} />
+                    key={u.id}
+                    unfollow={props.unfollow}
+                    follow={props.follow} />
                 )
             }
         </div>
